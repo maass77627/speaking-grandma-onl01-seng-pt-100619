e1fee64c0901342e594a69947214c011cccc06bf
kelly r maass <kmaass77627@gmail.com>
2020-04-02 02:56:18 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 
 def speak_to_grandma(string)
   if #{string} == #{string}.upcase
-      puts NO, NOT SINCE 1938!
+      puts "NO, NOT SINCE 1938!"
     else
   puts "HUH?! SPEAK UP, SONNY!"
 elseif #{string} == "I LOVE YOU GRANDMA!"
