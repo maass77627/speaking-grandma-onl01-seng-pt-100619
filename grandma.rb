702bc989c7f5610f2b2361d455be60650763381f
kelly r maass <kmaass77627@gmail.com>
2020-04-02 02:55:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,4 @@
-# Write a speak_to_grandma method.
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
 def speak_to_grandma(string)
   if #{string} == #{string}.upcase
       puts NO, NOT SINCE 1938!
