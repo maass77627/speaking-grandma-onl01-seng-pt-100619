dca550ff205f4ee9553f77f465907d1442abf05d
kelly r maass <kmaass77627@gmail.com>
2020-04-02 03:07:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 
 def speak_to_grandma(string)
   if string == string.upcase
-      puts "NO, NOT SINCE 1938!"
+     "NO, NOT SINCE 1938!"
     else
-  puts "HUH?! SPEAK UP, SONNY!"
+   "HUH?! SPEAK UP, SONNY!"
 elsif string == "I LOVE YOU GRANDMA!"
-puts "I LOVE YOU TOO PUMPKIN!"
+ "I LOVE YOU TOO PUMPKIN!"
 end
 end
